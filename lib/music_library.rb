6a4c691df7214cd4e577c65c0afd666561e19564
aurel fontaine <ajfame11@gmail.com>
2020-05-04 20:34:12 +0000

    Automatically backed up by Learn

--- lib/music_library.rb
+++ lib/music_library.rb
@@ -69,7 +69,7 @@ def list_songs
     input = gets.strip
     if (1..Song.all.length).include?(input)
       song = Song.all.sort_by(&:name)[input -1]
-      puts "Playing #{song.name} by #{song.artist.name}"
+      puts "Playing #{song.name} by #{song.artist.name}" if song
     end
   end 
 end
